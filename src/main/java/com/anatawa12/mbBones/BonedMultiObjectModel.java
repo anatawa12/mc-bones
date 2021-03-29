70200299699af1345115d860efc9b2f5ddfc3f72
anatawa12 <anatawa12@icloud.com>
2021-03-29 19:31:40 +0900

    add: BonedMultiObjectModel

---
+++ src/main/java/com/anatawa12/mbBones/BonedMultiObjectModel.java
@@ -0,0 +1,44 @@
+package com.anatawa12.mbBones;
+
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableMap;
+import org.jetbrains.annotations.NotNull;
+
+public class BonedMultiObjectModel {
+    private final ImmutableMap<String, BonedObject> map;
+    public final @NotNull BoneTree boneTree;
+
+    public BonedMultiObjectModel(Builder builder) {
+        this.map = builder.builder.build();
+        this.boneTree = builder.tree;
+    }
+
+    public static Builder builder(BoneTree tree) {
+        return new Builder(tree);
+    }
+
+    public BonedObject getByName(String name) {
+        return map.get(name);
+    }
+
+    public ImmutableCollection<BonedObject> getObjects() {
+        return map.values();
+    }
+
+    public static class Builder {
+        public final BoneTree tree;
+        private final ImmutableMap.Builder<String, BonedObject> builder = ImmutableMap.builder();
+
+        private Builder(BoneTree tree) {
+            this.tree = tree;
+        }
+
+        public void addObject(BonedObject obj) {
+            builder.put(obj.name, obj);
+        }
+
+        public BonedMultiObjectModel build() {
+            return new BonedMultiObjectModel(this);
+        }
+    }
+}
